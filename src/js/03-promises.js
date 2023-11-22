de96e6d57ccb04975c2d187e2da14fcedb1a9b58
Yevhenii Datsenko <yevheniydatsenko@gmail.com>
2023-11-22 22:16:32 +0200

    Task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,58 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onSubmitForm);
+
+async function onSubmitForm(event) {
+  event.preventDefault();
+  const { delay, step, amount } = event.currentTarget.elements;
+
+  if (delay.value < 0 || step.value < 0 || amount.value < 0) {
+    iziToast.show({
+      title: 'Warning',
+      message: `❗ Please enter a positive number`,
+      position: 'topCenter',
+      color: 'yellow',
+    });
   } else {
-    // Reject
+    for (let i = 0; i < amount.value; i++) {
+      const position = i + 1;
+      const delays = Number(delay.value) + step.value * i;
+
+      try {
+        await createPromise(position, delays);
+        iziToast.show({
+          title: 'Success',
+          message: `✅ Fulfilled promise ${position} in ${delays}ms`,
+          position: 'topRight',
+          color: 'green',
+        });
+      } catch (error) {
+        iziToast.show({
+          title: 'Error',
+          message: `❌ Rejected promise ${position} in ${delays}ms`,
+          position: 'topRight',
+          color: 'red',
+        });
+      }
+    }
   }
+
+  event.currentTarget.reset();
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
